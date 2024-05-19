5d7b93664ce2c33df71ae31087037c67602a18da
luisbandivas <jimboybandivas@gmail.com>
2024-05-19 17:57:27 +0800

    adsasd

--- aibeesion/vite.config.js
+++ aibeesion/vite.config.js
@@ -6,7 +6,11 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["react-fast-marquee", "react-loader-spinner"],
+      external: [
+        "react-fast-marquee",
+        "react-loader-spinner",
+        "@google/generative-ai",
+      ],
     },
   },
 });
